8b68cd8dab985608aa4587951a4328e1ade9ae7c
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-07-05 20:00:56 +0200

    Ecriture du template admin.php et écrire code require pour le template

--- ip-function.php
+++ ip-function.php
@@ -4,16 +4,19 @@
  * Ajout d'un nouveau lien dans le menu d'administration
  * /
 
-// Hook the 'admin_menu' action hook, run the function named 'mfp_Add_My_Admin_Link()'*/
-add_action( 'admin_init', 'ip_Add_My_Admin_Link' );
+// J'ajoute un hook pour afficher le lien de mon extention*/
+add_action( 'admin_menu', 'ip_Add_My_Admin_Link');
 
  // Ajout d'un nouveau lien dans le menu*/
  function ip_Add_My_Admin_Link() {
      add_menu_page (
-         'Option', //Titre de la page
+         'Réglages', //Titre de la page
          'Info-Pack', //Texte du lien du menu
          'manage_options', //Exigence de capacité pour voir le lien
-         'Includes/info-pack.php' // Le 'slug' - fichier à afficher en cliquant sur le lien
+         'admin/admin.php' // Le 'slug' - fichier à afficher en cliquant sur le lien
      );
  }
 
+ function Info_pack_admin_page() {
+    require_once ('../admin.php');
+}
