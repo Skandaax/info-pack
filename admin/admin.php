b82629750fc0503f3198aefd5ece9640d5456197
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-07-06 06:41:38 +0200

    modification

--- admin/admin.php
+++ admin/admin.php
@@ -1,17 +1,20 @@
 
     <div class="wrap">
         <ul class="nav nav-tabs">
-        <li class="nav-item">
-            <a class="nav-link active" href="#">Active</a>
-        </li>
-    <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-    </li>
-        <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Général</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Plan de site</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">My mails</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">My Gmap</a>
+            </li>
+            <li class="nav-item">
+                 <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
         </ul>
     </div>
--- ip-function.php
+++ ip-function.php
@@ -10,7 +10,7 @@ add_action( 'admin_menu', 'ip_Add_My_Admin_Link');
  // Ajout d'un nouveau lien dans le menu d'administration*/
  function ip_Add_My_Admin_Link() {
      add_menu_page (
-         'Reglage', //Titre de la page
+         'Info-Pack', //Titre de la page
          'Info-Pack', //Texte du lien du menu
          'manage_options', //Exigence de capacité pour voir le lien
          'info-pack/admin/admin.php' // Le 'slug' - fichier à afficher en cliquant sur le lien
