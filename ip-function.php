5a6efcf3cb57b24e4e257a165f5b4f3135207015
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-07-06 06:29:06 +0200

    modification ip-function.php

--- ip-function.php
+++ ip-function.php
@@ -7,16 +7,18 @@
 // J'ajoute un hook pour afficher le lien de mon extention*/
 add_action( 'admin_menu', 'ip_Add_My_Admin_Link');
 
- // Ajout d'un nouveau lien dans le menu*/
+ // Ajout d'un nouveau lien dans le menu d'administration*/
  function ip_Add_My_Admin_Link() {
      add_menu_page (
-         'Réglages', //Titre de la page
+         'Reglage', //Titre de la page
          'Info-Pack', //Texte du lien du menu
          'manage_options', //Exigence de capacité pour voir le lien
-         'admin/admin.php' // Le 'slug' - fichier à afficher en cliquant sur le lien
+         'info-pack/admin/admin.php' // Le 'slug' - fichier à afficher en cliquant sur le lien
      );
  }
 
+ //Le fichier d'inclusion "admin.php", le template qui sera afficher dans le menu "Info-Pack"
  function Info_pack_admin_page() {
-    require_once ('../admin.php');
+    require_once ('/admin.php');
 }
+
