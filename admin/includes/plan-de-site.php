088ac679a265727fc56728d9d0eabe7c89cfe71c
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-07-16 11:42:40 +0200

    modification

--- admin/includes/plan-de-site.php
+++ admin/includes/plan-de-site.php
@@ -11,12 +11,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 //Cette fonction sert à relier le fichier CSS au shortcode [plan_du_menu]
 function add_css() {
-    wp_register_style('plan_menu', plugins_url('css/styles.css', __FILE__));
+    wp_register_style('plan_menu', plugins_url('../css/styles.css', __FILE__));
     wp_enqueue_style('plan_menu');
 }
 
 // Fonction qui va permettre d'afficher le menu en lien textuel.
-function Plan_Menu( $atts, $content=null ) {
+function Plan_Menu( $atts, $content=null) {
     ob_start();
 
     //je recupere les parametres du shortcode
@@ -46,8 +46,43 @@ function Plan_Menu( $atts, $content=null ) {
 }
 
 //Sortcode a mettre dans l'une des pages de son site pour faire apparaitre le plan du menu
-// add_shortcode('plan_du_menu', 'Plan_Menu');
+//[plan_du_menu]
 add_shortcode('plan_du_menu', 'Plan_Menu');
 
 //*******************************************************************************/
+// Fonction qui va me permettre d'afficher le les catégories d'aticles.
+function categorie($atts, $content=null) {
+    ob_start();
 
+    //je recupere les parametres du shortcode
+    
+    extract(shortcode_atts(array(
+        'numberposts' => 5, //Nombre de méssages à récuperer
+        'category' => 0, //ID de catégories ou liste d'ID séparé par des virgules.
+        'orderby' => 'date', //
+        'order' => 'DESC',
+        'post_type' => 'post',
+    ), $atts, 'categorie'));
+
+    global $post;
+    $tmp_post = $post;
+    $myposts = get_posts('showposts=' .$numberposts.'$orderby'.'&category='.$category);
+    $out = '<ul>';
+    foreach($myposts as $post); {
+        setup_postdata($post); 
+
+        $out .= '<li><h3><a href="'.get_permalink().'">'.the_title('','',false).'</a></h3></li>';
+        $out .= '<a href="'.get_permalink().'">'.get_the_post_thumbnail($id ='', $attr = '').'</a>';
+        $out .= get_post_type($post->ID);
+        $out .='<p><a href="'.get_permalink().'">Lire la suite... </a></p>';
+        $out .= '<hr />';
+        $out .= '</li>';
+
+    };
+    wp_reset_postdata();
+    $post = $tmp_post;
+    return $out;  
+
+}
+
+add_shortcode('articles', 'categorie');
